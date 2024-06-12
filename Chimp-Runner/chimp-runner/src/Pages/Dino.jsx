c3727964b98f4fdb5be1e3c78b6aa618a7a292a6
JuanseFisaTS <142258092+JuanseFisaTS@users.noreply.github.com>
2024-06-11 23:10:08 -0500

    Enemigo Volador

--- Chimp-Runner/chimp-runner/src/Pages/Dino.jsx
+++ Chimp-Runner/chimp-runner/src/Pages/Dino.jsx
@@ -7,9 +7,8 @@ import Popup from '../Components/popUp';
 import { getFirestore, collection, addDoc } from 'firebase/firestore';
 
 function saveScoreToFirestore(db, username, score) {
-  // Utiliza las funciones de Firestore para guardar el score
   addDoc(collection(db, 'Usuarios'), {
-    username: username, // Pass the username parameter
+    username: username,
     score: score
   })
   .then((docRef) => {
@@ -20,7 +19,6 @@ function saveScoreToFirestore(db, username, score) {
   });
 }
 
-
 function Dino() {
   const { username: usernameFromContext, db } = useContext(UserContext);
   useEffect(() => {
@@ -29,12 +27,14 @@ function Dino() {
   const [username, setUsername] = useState('');
   const location = useLocation();
   const dinoRef = useRef();
-  const cactusRef = useRef();
+  const obstacleRef = useRef();
   const [score, setScore] = useState(0);
   const [obstacleSpeed, setObstacleSpeed] = useState(10);
-  const speedIncrement = 5000;
   const [isGameOver, setIsGameOver] = useState(false);
-  const cactusAnimationIntervalRef = useRef(null);
+  const [obstacleAnimationStarted, setObstacleAnimationStarted] = useState(false);
+  const [isFlying, setIsFlying] = useState(false);
+  const obstacleAnimationIntervalRef = useRef(null);
+  const [isJumping, setIsJumping] = useState(false);
 
   useEffect(() => {
     const searchParams = new URLSearchParams(location.search);
@@ -42,36 +42,31 @@ function Dino() {
     setUsername(usernameFromURL);
   }, [location.search]);
 
-  
-  // Función para hacer saltar al dinosaurio
   const jump = () => {
-    if (!!dinoRef.current && dinoRef.current.classList !== 'jump') {
+    if (!!dinoRef.current && !isJumping) {
+      setIsJumping(true);
       dinoRef.current.classList.add('jump');
       setTimeout(function () {
         dinoRef.current.classList.remove('jump');
+        setIsJumping(false); // Una vez que termine el salto, establecer a falso
       }, 300);
     }
   };
 
- 
-
   useEffect(() => {
     const isAlive = setInterval(function () {
-      // Obtener la posición Y actual del dinosaurio
-      const dinoTop = parseInt(
-        getComputedStyle(dinoRef.current).getPropertyValue('top')
-      );
-  
-      // Obtener la posición X actual del cactus
-      let cactusLeft = parseInt(
-        getComputedStyle(cactusRef.current).getPropertyValue('left')
-      );
-  
-      // Detectar colisión con cactus
-      if (cactusLeft < 40 && cactusLeft > 0 && dinoTop >= 140) {
-        // Colisión
-        setIsGameOver(true); // Cambiar el estado a true para mostrar el Popup
-        clearInterval(isAlive); // Detener el intervalo
+      const dinoTop = parseInt(getComputedStyle(dinoRef.current).getPropertyValue('top'));
+      const obstacleLeft = parseInt(getComputedStyle(obstacleRef.current).getPropertyValue('left'));
+      const obstacleTop = isFlying ? 60 : 150;
+    
+      const dinoHeight = 50; // Altura del dinosaurio
+      const obstacleHeight = 40; // Altura del cactus
+      const dinoBottom = dinoTop + dinoHeight;
+      const obstacleBottom = obstacleTop + obstacleHeight;
+    
+      if (!isJumping && obstacleLeft < 40 && obstacleLeft > 0 && dinoBottom >= obstacleTop && dinoTop <= obstacleBottom) {
+        setIsGameOver(true);
+        clearInterval(isAlive);
       } else {
         setScore((prevScore) => {
           if (prevScore >= 1000 && prevScore % 50 === 0) {
@@ -80,104 +75,85 @@ function Dino() {
           return prevScore + 1;
         });
       }
-      
     }, obstacleSpeed);
+    
   
-    // Limpieza del intervalo al desmontar el componente
     return () => {
       clearInterval(isAlive);
     };
-  }, [obstacleSpeed]); // Solo depende de obstacleSpeed
+  }, [obstacleSpeed, isJumping, isFlying]);
   
-  // useEffect para la animación de los cactus
+
   useEffect(() => {
-    // Iniciar la animación solo si el juego no ha terminado
-    if (!isGameOver) {
-      const cactusAnimationInterval = setInterval(() => {
-        // Aquí va la lógica para animar los cactus
-        // Por ejemplo, mover los cactus a la izquierda
-      }, 100); // Ajusta el intervalo según tu animación
+    if (!isGameOver && obstacleAnimationStarted) {
+      const obstacleAnimationInterval = setInterval(() => {
+        setIsFlying(Math.random() < 0.5); // 50% de probabilidad de ser volador
+      }, 2000); // Cambiar cada 2 segundos
   
-      // Limpieza del intervalo al desmontar el componente o al finalizar el juego
       return () => {
-        clearInterval(cactusAnimationInterval);
+        clearInterval(obstacleAnimationInterval);
       };
     }
-  }, [isGameOver]); // Dependencia solo para iniciar la animación y limpiar el intervalo al finalizar el juego
-  
-  useEffect(() => {
-    // Iniciar la animación solo si el juego no ha terminado
-    if (!isGameOver) {
-      const cactusAnimationInterval = setInterval(() => {
-        // Aquí va la lógica para animar los cactus
-        // Por ejemplo, mover los cactus a la izquierda
-      }, 100); // Ajusta el intervalo según tu animación
-  
-      // Guardar la referencia del intervalo para poder limpiarlo más tarde
-      cactusAnimationIntervalRef.current = cactusAnimationInterval;
-    }
-  
-    // Limpieza del intervalo al finalizar el juego
-    return () => {
-      clearInterval(cactusAnimationIntervalRef.current);
-    };
-  }, [isGameOver]); // Dependencia para iniciar la animación y limpiar el intervalo al finalizar el juego
-  
+  }, [isGameOver, obstacleAnimationStarted]);
 
-  // useEffect para agregar el evento de salto dependiendo del tipo de dispositivo
+  useEffect(() => {
+    setTimeout(() => {
+      setObstacleAnimationStarted(true);
+    }, 2000);
+  }, []);
+
   useEffect(() => {
     const handleJump = () => {
-      jump();
+      if (!isGameOver) jump();
     };
-
-    // Detectar el tipo de dispositivo a través del User-Agent
+  
     const isMobileDevice = /Mobi|Android/i.test(navigator.userAgent);
-
-    // Si es un dispositivo móvil, agregar evento de salto al tocar la pantalla
+  
     if (isMobileDevice) {
       document.addEventListener('touchstart', handleJump);
     } else {
-      // Si es una computadora, agregar evento de salto con la barra espaciadora
       document.addEventListener('keydown', (event) => {
         if (event.keyCode === 32) {
-          jump();
+          handleJump();
         }
       });
     }
-
+  
     return () => {
       document.removeEventListener('touchstart', handleJump);
       document.removeEventListener('keydown', handleJump);
     };
-  }, []);
+  }, [isGameOver]);
+  
 
   useEffect(() => {
-    if (isGameOver && db && username!=null && score) {
-      saveScoreToFirestore(db, username, score); // Pass the username
+    if (isGameOver && db && username && score) {
+      saveScoreToFirestore(db, username, score);
     }
   }, [isGameOver, db, username, score]);
-  
-  
-  
+
   return (
     <div className="container">
-      <div className="ad top">Anuncio Superior</div>
+
       <h1 className="title">Chimp Runner</h1>
-      <h2 className="welcome-message">Bienvenido {usernameFromContext || username} </h2> {/* Mensaje de bienvenida */}
+      <h2 className="welcome-message">Bienvenido {usernameFromContext || username} </h2>
       <div className="game">
         Score : {score}
         <div id="dino" ref={dinoRef}></div>
-        <div id="cactus" ref={cactusRef}></div>
+        <div
+          id="obstacle"
+          ref={obstacleRef}
+          className={isFlying ? 'flying' : 'cactus'}
+          style={{ animation: obstacleAnimationStarted ? 'block 1s infinite linear' : 'none' }}
+        ></div>
       </div>
       {isGameOver && <Popup username={usernameFromContext || username} score={score} />}
- {/* Renderizar el Popup si el juego ha terminado */}
       <Link to="/leaders">
         <button className="play-button">Ir a LeaderBoard</button>
       </Link>
-      <div className="ad bottom">Anuncio Inferior</div>
+     
     </div>
   );
-  
 }
 
 export default Dino;
